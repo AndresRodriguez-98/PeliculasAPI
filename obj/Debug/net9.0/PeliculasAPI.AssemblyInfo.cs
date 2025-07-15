5468ae8bcb68cfa6536fef006fcc04cbadb62f80
ReloJunior <andresrodriguezab@hotmail.com>
2025-07-15 00:35:12 -0300

    fix secrets

--- obj/Debug/net9.0/PeliculasAPI.AssemblyInfo.cs
+++ obj/Debug/net9.0/PeliculasAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PeliculasAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+19214396a829a8fc2ba5d020c35462dd1ab66e9d")]
 [assembly: System.Reflection.AssemblyProductAttribute("PeliculasAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PeliculasAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
